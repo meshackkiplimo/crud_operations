16214dc68cb89bc67478305dcbb06cdcab311d88
meshack kimaiyo <meshackkimaiyo5@gmail.com>
2024-12-15 16:16:28 +0300

    db connction

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express')
 const app = express()
+const mongoose= require('mongoose')
+require('dotenv').config();
+const mongoURI = process.env.MONGODB_URI;
+
+
+
+
 
 app.listen(3004,()=>{
     console.log("Server is running on http://localhost:3004")
@@ -8,3 +15,12 @@ app.listen(3004,()=>{
 app.get('/',(req,res)=>{
     res.send("Hello World")
 })
+
+mongoose.connect(mongoURI)
+  .then(() => {
+    
+    console.log('Connected to database !')})
+
+  .catch(() => {
+    console.log("Error connecting to database")
+   })
